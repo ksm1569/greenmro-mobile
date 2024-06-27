736da6333a2dab298498a056728536f219f672ad
kimsumin <tnals1564@naver.com>
2024-06-27 10:19:54 +0900

    상품 상세보기 - 상품의 카테고리 정보 view에 적용

--- src/main/resources/static/js/common/common.js
+++ src/main/resources/static/js/common/common.js
@@ -1,3 +1,14 @@
+function apiCall(apiUrl, callback) {
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => callback(data))
+        .catch(error => console.error('Error fetching product details:', error));
+}
 
 /**
  * Fetches product details from the server and executes a callback function with the response data.
@@ -8,13 +19,17 @@ function fetchProductDetails(prefItem, callback) {
     const baseUrl = window.location.origin;
     const apiUrl = `${baseUrl}/api/products/${prefItem}`;
 
-    fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => callback(data))
-        .catch(error => console.error('Error fetching product details:', error));
+    apiCall(apiUrl, callback);
+}
+
+/**
+ * Fetches product details from the server and executes a callback function with the response data.
+ * @param {*[][]} prefItem - The product ID to fetch details for.
+ * @param {function} callback - A callback function to execute with the fetched product data.
+ */
+function fetchProductCategory(prefItem, callback) {
+    const baseUrl = window.location.origin;
+    const apiUrl = `${baseUrl}/api/products/${prefItem}/categories`;
+
+    apiCall(apiUrl, callback);
 }
